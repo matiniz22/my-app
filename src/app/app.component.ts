2a64fafbdf8622fded5a925425144cd6e42c1425
matiniz22 <matiniz@hotmail.com>
2023-07-02 19:12:21 -0300

    directivas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+  title = 'listado de Alumnos de angular';
+
+
+
+ birthday = new Date ()
+
 }
+
+
